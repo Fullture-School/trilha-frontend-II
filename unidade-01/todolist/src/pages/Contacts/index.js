e3964f1e8008042695cb4baa382bc8868544ddef
Rafael Tomé de Souza <rafaeltomesouza@gmail.com>
2021-03-23 19:50:31 -0300

    Fullture-School - first commit

--- unidade-01/todolist/src/pages/Contacts/index.js
+++
@@ -1,2 +0,0 @@
-import { default as Contacts } from './Contacts';
-export { Contacts };
--- unidade-01/todolist/src/pages/ToDoList/index.js
+++
@@ -1,5 +0,0 @@
-import { default as ToDoList } from './ToDoList';
-export { ToDoList };
-
-//import { default as ToDoList } from './ToDoList-hooks';
-//export { ToDoList };
